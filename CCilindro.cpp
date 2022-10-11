e48db79cf7650d4f2307a0367f8909b3aa914381
Hildiu <31174932+Hildiu@users.noreply.github.com>
2022-10-11 10:52:41 -0500

    Add files via upload

---
+++ CCilindro.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Profesor08 on 11/10/2022.
+//
+
+#include "CCilindro.h"
---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*---------------------------------------------
+ * Dato de Entrada: radio, altura (double)
+ * Dato de Salida: aSupCilindrica, aLateral,
+ *                 aTotal, volumen  (double)
+ ---------------------------------------------*/
+
+#include <iostream>
+
+int main()
+{
+
+    return 0;
+}
